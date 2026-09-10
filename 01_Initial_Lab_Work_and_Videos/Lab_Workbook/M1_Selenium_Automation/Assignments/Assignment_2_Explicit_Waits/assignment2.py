34db02578c6fe70f87b876a4498affddf4d8b73f
Arpan Mukherjee <arpanmukherjeekw@gmail.com>
2026-09-10 21:10:19 +0530

    Implement Selenium explicit waits for form fields

--- 01_Initial_Lab_Work_and_Videos/Lab_Workbook/M1_Selenium_Automation/Assignments/Assignment_2_Explicit_Waits/assignment2.py
+++ 01_Initial_Lab_Work_and_Videos/Lab_Workbook/M1_Selenium_Automation/Assignments/Assignment_2_Explicit_Waits/assignment2.py
@@ -1,1 +1,51 @@
+from selenium import webdriver
 
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
+browsername = "chrome"
+
+if browsername.lower() == "chrome":
+    driver=webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+elif browsername.lower()=="firefox":
+    driver=webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+else:
+    raise Exception("Invalid browser name.")
+
+
+driver.get("https://testautomationpractice.blogspot.com/")
+driver.maximize_window()
+
+
+# Create explicit wait
+wait = WebDriverWait(driver, 10)
+
+# Wait until Name field is visible
+name = wait.until(
+    EC.visibility_of_element_located((By.ID, "name"))
+)
+# Enter name
+name.send_keys("Arpan Mukherjee")
+print("Name entered successfully")
+
+
+# Wait until Email field is clickable
+email = wait.until(
+    EC.element_to_be_clickable((By.ID, "email"))
+)
+# Enter email
+email.send_keys("arpan@example.com")
+print("Email entered successfully")
+
+driver.quit()
