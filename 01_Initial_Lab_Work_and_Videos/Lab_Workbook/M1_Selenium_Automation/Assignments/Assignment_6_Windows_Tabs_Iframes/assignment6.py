d6bef11bdabdeeefa7be728d5a31e70cff63cc07
Arpan Mukherjee <arpanmukherjeekw@gmail.com>
2026-09-13 18:12:31 +0530

    Implement Selenium automation for iframe and tabs

--- 01_Initial_Lab_Work_and_Videos/Lab_Workbook/M1_Selenium_Automation/Assignments/Assignment_6_Windows_Tabs_Iframes/assignment6.py
+++ 01_Initial_Lab_Work_and_Videos/Lab_Workbook/M1_Selenium_Automation/Assignments/Assignment_6_Windows_Tabs_Iframes/assignment6.py
@@ -1,1 +1,96 @@
+from selenium import webdriver
 
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
+browsername = "chrome"
+
+if browsername.lower() == "chrome":
+    driver=webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+elif browsername.lower()=="firefox":
+    driver=webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+else:
+    raise Exception("Invalid browser name.")
+
+
+driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe")
+driver.maximize_window()
+
+
+print("Main page title:", driver.title)
+print("Main window:", driver.current_window_handle)
+
+
+
+# Iframe handling
+iframe = driver.find_element(By.XPATH, "//iframe[@id='iframeResult']")
+
+driver.switch_to.frame(iframe)
+print("Switched to iframe successfully")
+
+driver.switch_to.default_content()
+print("Switched back to main page")
+print()
+
+
+
+# Open new tab
+driver.switch_to.new_window("tab")
+driver.get("https://www.google.com")
+
+print("New tab opened")
+print("New tab title:", driver.title)
+print("New tan handle:", driver.current_window_handle)
+print()
+
+
+
+# Get all window handles
+handles = driver.window_handles
+print("Total windows/tabs:", len(handles))
+
+for handle in handles:
+    print("Window handles:", handle)
+
+print()
+
+
+
+# Switch back to main window
+driver.switch_to.window(handles[0])
+print("Switched back to main window")
+print("Main window title:", driver.title)
+print()
+
+
+
+# Switch to new tab and close it
+driver.switch_to.window(handles[1])
+print("Switched to new tab")
+driver.close()
+print("New tan closed")
+print()
+
+
+
+# Return to main window
+driver.switch_to.window(handles[0])
+print("Returned to main window")
+print("Final window title:", driver.title)
+
+
+
+time.sleep(5)
+driver.quit()
